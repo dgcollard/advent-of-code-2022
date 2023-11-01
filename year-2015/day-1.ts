5d4e805996f00dea1c09a2d75569d90557ac1636
David Collard <davidcollard@gocardless.com>
2023-11-01 14:31:41 +0000

    year 2015 day 1

---
+++ year-2015/day-1.ts
@@ -0,0 +1,53 @@
+import { runWithInputFile, sum } from "../util";
+
+function parseInput(input: string): string[] {
+  const result = input.split("");
+
+  return result;
+}
+
+export function solve1(chars: string[]): number {
+  let floor = 0;
+
+  for (let i = 0; i < chars.length; i++) {
+    const char = chars[i];
+
+    if (char === "(") {
+      floor++;
+    }
+    if (char === ")") {
+      floor--;
+    }
+  }
+
+  return floor;
+}
+
+export function solve2(chars: string[]): number {
+  let floor = 0;
+
+  for (let i = 0; i < chars.length; i++) {
+    const char = chars[i];
+
+    if (char === "(") {
+      floor++;
+    }
+    if (char === ")") {
+      floor--;
+    }
+
+    if (floor < 0) {
+      return i + 1;
+    }
+  }
+
+  return -1;
+}
+
+function main(input: string) {
+  const parsedInput = parseInput(input);
+  console.log(solve1(parsedInput));
+  console.log(solve2(parsedInput));
+}
+
+runWithInputFile(main, "year-2015/day-1");
